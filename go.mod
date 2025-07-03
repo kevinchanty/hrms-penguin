615b30d4c519b58c8216b4aa8d311035dd172c77
Kevin <tychan@shoalter.com>
2025-07-03 18:26:34 +0800

    WIP rewrite 2

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/PuerkitoBio/goquery v1.8.1
 	github.com/charmbracelet/log v0.3.1
 	github.com/gen2brain/beeep v0.0.0-20240112042604-c7bb2cd88fea
+	github.com/joho/godotenv v1.5.1
+	github.com/spf13/cobra v1.9.1
 	golang.org/x/net v0.20.0
 )
 
@@ -15,7 +17,9 @@ require (
 	github.com/charmbracelet/lipgloss v0.9.1 // indirect
 	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 )
--- main.go
+++ main.go
@@ -1,61 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"context"
-	"flag"
-	"fmt"
-	"os"
-	"time"
-
-	"github.com/gen2brain/beeep"
-)
-
-const defaultTick = 15 * time.Second
-
-type config struct {
-	tick time.Duration
-}
-
-func (c *config) init(args []string) error {
-	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
-
-	tick := flags.Duration("tick", defaultTick, "Ticking interval")
-
-	if err := flags.Parse(args[1:]); err != nil {
-		return err
-	}
-
-	c.tick = *tick
-	return nil
-}
+import "hrms-penguin/cmd"
 
 func main() {
-	fmt.Println("haha")
-
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-
-	c := &config{}
-
-	defer func() {
-		cancel()
-	}()
-
-	if err := run(ctx, c); err != nil {
-		os.Exit(1)
-	}
-	// beeep.Notify("haha", "testing", "")
-}
-
-func run(ctx context.Context, c *config) error {
-	c.init(os.Args)
-
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case <-time.Tick(c.tick):
-			beeep.Notify("Kirby is talking", "Poyo!", "")
-		}
-	}
+	cmd.Execute()
 }
